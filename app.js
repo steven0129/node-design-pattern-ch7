5c9b0029ca689a7c412d850876603196e73c327f
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-15 20:19:56 +0800

    service locator pattern

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const bodyParser = require('body-parser');
 const errorHandler = require('errorhandler');
 const http = require('http');
 
-const authController = require('./lib/authController');
-
-let app = module.exports = new Express();
+const app = module.exports = new Express();
 app.use(bodyParser.json());
+
+const svcLoc = require('./lib/serviceLocator')();
+
+svcLoc.register('dbName', 'example-db');
+svcLoc.register('tokenSecret', 'SHHH!');
+svcLoc.factory('db', require('./lib/db'));
+svcLoc.factory('authService', require('./lib/authService'));
+svcLoc.factory('authController', require('./lib/authController'));
+
+const authController = svcLoc.get('authController');
 
 app.post('/login', authController.login);
 app.get('/checkToken', authController.checkToken);
