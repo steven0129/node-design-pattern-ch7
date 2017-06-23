00edfcc54e200f4d26456416a7f37eb2cdc6d53b
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-23 11:28:55 +0800

    重置插件位置

---
+++ authsrv-plugin-logout/main.js
@@ -0,0 +1,35 @@
+const parentRequire = module.parent.require
+
+const authService = parentRequire('./lib/authService')
+const db = parentRequire('./lib/db')
+const app = parentRequire('./app')
+const tokensDB = db.sublevel('tokens')
+
+const oldLogin = authService.login
+authService.login = (username, password, callback) => {
+    oldLogin(username, password, (err, token) => {
+        if (err) return callback(err)
+
+        tokensDB.put(token, { username: username }, () => {
+            callback(null, token)
+        })
+    })
+}
+
+const oldCheckToken = authService.checkToken
+authService.checkToken = (token, callback) => {
+    tokensDB.get(token, (err, res) => {
+        if (err) return callback(err)
+        oldCheckToken(token, callback)
+    })
+}
+
+authService.logout = (token, callback) => {
+    tokensDB.del(token, callback)
+}
+
+app.get('/logout', (req, res, next) => {
+    authService.logout(req.query.token, () => {
+        res.status(200).send({ ok: true })
+    })
+})
